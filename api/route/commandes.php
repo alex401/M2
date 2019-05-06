f5f95489357b340c9390851a03d99ec690d80752
Nennya <darkzorglub@hotmail.com>
2019-05-06 16:39:00 +0200

    Add chantier in search filter.

--- api/route/commandes.php
+++ api/route/commandes.php
@@ -89,40 +89,48 @@ $app->post('/v1/search/commandes', function ($request,$response) {
   $nom = $command['client'];
   $nom = "%".$nom."%";
   $dateEnvoi = $command['date'];
+  $chantier = $command['chantier'];
+  if($chantier == "") {
+    $chantier = "%%";
+  }
 
   $sth;
 
-  // If rowid is specified, there should be only one result.
+  // If rowid is specified, there should be only one result. TODO change logic like for chantier.
   if($rowid != "" && $dateEnvoi !="") {
-    $sth = $this->dbm2->prepare("SELECT * FROM commandes WHERE rowid = :rowid AND type like :type
+    $sth = $this->dbm2->prepare("SELECT * FROM commandes WHERE rowid = :rowid AND type like :type AND chantier LIKE :chantier
       AND nom like :nom AND DATE(timestampDate) >= :dateEnvoi AND DATE(timestampDate) < DATE_ADD(:dateEnvoi, INTERVAL 1 DAY)");
 
     $sth->bindParam(':rowid', $rowid, PDO::PARAM_INT);
     $sth->bindParam(':type', $type, PDO::PARAM_STR);
     $sth->bindParam(':nom', $nom, PDO::PARAM_STR);
     $sth->bindParam(':dateEnvoi', $dateEnvoi, PDO::PARAM_STR);
+    $sth->bindParam(':chantier', $chantier, PDO::PARAM_STR);
 
   } else if($rowid != "") {
-    $sth = $this->dbm2->prepare("SELECT * FROM commandes WHERE rowid = :rowid AND type like :type
+    $sth = $this->dbm2->prepare("SELECT * FROM commandes WHERE rowid = :rowid AND type like :type AND chantier LIKE :chantier
       AND nom like :nom");
 
     $sth->bindParam(':rowid', $rowid, PDO::PARAM_INT);
     $sth->bindParam(':type', $type, PDO::PARAM_STR);
     $sth->bindParam(':nom', $nom, PDO::PARAM_STR);
+    $sth->bindParam(':chantier', $chantier, PDO::PARAM_STR);
 
   } else if($dateEnvoi !="") {
-    $sth = $this->dbm2->prepare("SELECT * FROM commandes WHERE type like :type
+    $sth = $this->dbm2->prepare("SELECT * FROM commandes WHERE type like :type AND chantier LIKE :chantier
       AND nom like :nom AND DATE(timestampDate) >= :dateEnvoi AND DATE(timestampDate) < DATE_ADD(:dateEnvoi, INTERVAL 1 DAY)");
 
     $sth->bindParam(':type', $type, PDO::PARAM_STR);
     $sth->bindParam(':nom', $nom, PDO::PARAM_STR);
     $sth->bindParam(':dateEnvoi', $dateEnvoi, PDO::PARAM_STR);
+    $sth->bindParam(':chantier', $chantier, PDO::PARAM_STR);
 
   } else {
-    $sth = $this->dbm2->prepare("SELECT * FROM commandes WHERE type like :type AND nom like :nom");
+    $sth = $this->dbm2->prepare("SELECT * FROM commandes WHERE type like :type AND nom like :nom AND chantier LIKE :chantier");
 
     $sth->bindParam(':type', $type, PDO::PARAM_STR);
     $sth->bindParam(':nom', $nom, PDO::PARAM_STR);
+    $sth->bindParam(':chantier', $chantier, PDO::PARAM_STR);
   }
 
   try {
--- src/js/controllers/commandes/commandeSearchCtrl.js
+++ src/js/controllers/commandes/commandeSearchCtrl.js
@@ -3,23 +3,45 @@ angular.module('PCIM2')
 
 function CommandeSearchCtrl($scope, $stateParams, $http) {
   $scope.commandes = {};
-  $scope.command = {id: "", type:"", client:"", date:""};
+  $scope.command = {id: "", type:"", client:"", date:"", chantier: ""};
+  $scope.chantier = "";
 
   $scope.search = function(command) {
     searchCommands(command);
   }
 
+  var Load = function () {
+    loadChantiers();
+  }
+
   var searchCommands = function(command) {
+    if($scope.chantier && $scope.chantier != null) {
+      command.chantier = $scope.chantier;
+    } else {
+      command.chantier = "";
+    }
     $http({
       method: 'POST',
       url: 'api/index.php/v1/search/commandes',
       data: {command: command}
     }).then(function successCallback(response) {
       $scope.commandes = response.data;
-      console.log(response.data);
+    }, function errorCallback(response) {
+      console.log(response);
+    });
+  }
+
+  var loadChantiers = function () {
+    $http({
+      method: 'GET',
+      url: 'api/index.php/v1/chantiers'
+    }).then(function successCallback(response) {
+      $scope.chantiers = response.data;
       }, function errorCallback(response) {
-        console.log(response);
+        console.log("error");
       });
   }
+
+  Load();
 
 }
