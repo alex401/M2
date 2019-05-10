d469458f3a148d67e481f4210388711709ed0589
Nennya <darkzorglub@hotmail.com>
2019-05-10 13:31:21 +0200

    Propose name in cmd creation.

--- src/js/controllers/commandes/commandeCreationCtrl.js
+++ src/js/controllers/commandes/commandeCreationCtrl.js
@@ -29,6 +29,19 @@ function CommandeCreationCtrl($scope, $stateParams, $http) {
       });
   }
 
+  $scope.loadTiers = function (nom) {
+    if(nom != null && nom.length > 2) {
+      return $http({
+        method: 'GET',
+        url: 'api/index.php/v1/admin/tiers/'+nom
+      }).then(function successCallback(response) {
+        return response.data;
+      }, function errorCallback(response) {
+        console.log("error");
+      });
+    }
+  }
+
   $scope.submit = function (type) {
 
     switch(type) {
