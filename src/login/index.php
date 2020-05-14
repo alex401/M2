cc3066f7098832cd26dbb59f6f5660e44e16d8a6
Ostralion <sebastien.demierre@yahoo.fr>
2020-05-14 09:32:25 +0200

    Mini-corrections langue

--- src/login/index.php
+++ src/login/index.php
@@ -10,7 +10,7 @@ $logout="";
 
 
 
-<html lang="en" ng-app="PCIM2">
+<html lang="fr" ng-app="PCIM2">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
